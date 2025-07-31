4033ac7aa1d3ec64eca8ff0e7ac57b77aedbf076
Jrosero21 <jnrosero@gmail.com>
2025-07-31 14:53:25 -0700

    Cart updates

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import Nav from "./components/Nav";
+import React, { useState, useEffect } from 'react'
 import Footer from './components/Footer'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Home from "./Pages/Home";
 import Books from "./Pages/Books";
 import { books } from "./data";
 import BookInfo from "./Pages/BookInfo";
+import Cart from "./Pages/Cart";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+setCart([...cart, {...book, quantity: 1}])
+  }
+
+  function changeQuantity(book, quantity) {
+    console.log(book.quantity)
+  }
+
+  useEffect(() => {
+    console.log(cart)
+  }, [cart])
+
   return (
     <Router>
     <div className="App">
       <Nav />
       <Route path="/" exact component={Home} />
       <Route path="/books" exact render={() => <Books books={books} />} />
-      <Route path="/books/1" render={() => <BookInfo books={books} />} />
+      <Route path="/books/:id" render={() => <BookInfo books={books} addToCart={addToCart} cart={ cart } />} />
+      <Route path="/cart" render={() => <Cart books={books} cart={ cart } changeQuantity={changeQuantity} />} />
       <Footer /> 
     </div>
     </Router>
--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ library.add(faBars, faCartShopping, faXmark, faBolt, faBookOpen, faTags, faStar,
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+
     <App />
-  </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
