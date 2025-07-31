2628e207269e6fbe321177ccdd88a83dd35d92b2
Jrosero21 <jnrosero@gmail.com>
2025-07-31 11:36:13 -0700

    first commit

---
+++ src/Pages/BookInfo.jsx
@@ -0,0 +1,27 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const BookInfo = ({ books }) => {
+  return (
+    <div id="books__body">
+        <main id="books__main">
+            <div className="books__container">
+                <div className="row">
+                    <div className="book__selected--top">
+                        <Link to="/books" className="book__link">
+                        <FontAwesomeIcon icon="fa-solid fa-arrow-left" />
+                        </Link>
+                        <Link to="/book">
+                        <h2 className="book__selected--title--top">Books</h2>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </main>
+      
+    </div>
+  )
+}
+
+export default BookInfo
---
+++ src/components/ui/Book.jsx
@@ -0,0 +1,51 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Link } from "react-router-dom"
+
+const Book = ({ book }) => {
+  return (
+    <div className="book">
+      <Link to="/books/1">
+        <figure className="book__img--wrapper">
+          <img src={book.url} alt="" />
+        </figure>
+      </Link>
+      <div className="book__title">
+        <Link to="/books/1" className="book__title--link">
+          {book.title}
+        </Link>
+      </div>
+      <div className="book__ratings">
+        {new Array(Math.floor(book.rating)).fill(0).map((_, index) => (
+          <FontAwesomeIcon
+            icon="fa-solid fa-star"
+            style={{ color: "#FFD43B" }}
+            key={index}
+          />
+        ))}
+        {Number.isInteger(book.rating) ? (
+          ""
+        ) : (
+          <FontAwesomeIcon
+            icon="fa-solid fa-star-half-stroke"
+            style={{ color: "#FFD43B" }}
+          />
+        )}
+      </div>
+      <div className="book__price">
+        {book.salePrice ? (
+          <>
+            <span className="book__price--normal">
+              ${book.originalPrice.toFixed(2)}
+            </span>
+            ${book.salePrice.toFixed(2)}
+          </>
+        ) : (
+          <> ${book.originalPrice.toFixed(2)} </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Book;
